5f8f75f4bd382b57f3e147dfb5dbbf8ce40497c3
Manuel <mediavillarodriguez@gmail.com>
2016-03-02 15:50:17 +0100

    Commit - Login and register functions were created

--- examsApp/app/login/login-controller.js
+++ examsApp/app/login/login-controller.js
@@ -15,5 +15,14 @@
   function LoginCtrl() {
     var vm = this;
     vm.ctrlName = 'LoginCtrl';
+    vm.user = null;
+    vm.submit = function (user) {
+      vm.user = angular.copy(user);
+      // to-do
+    };
+    vm.register = function (register) {
+      vm.user = angular.copy(register);
+      // to-do
+    };
   }
 }());
--- examsApp/app/login/login-routes.js
+++ examsApp/app/login/login-routes.js
@@ -8,20 +8,14 @@
   function config($stateProvider) {
     $stateProvider
         .state('login', {
-            parent:'home',
-            views:{
-                'loginView@home':{
-                    templateUrl: 'login/login.tpl.html',
-                    controller: 'LoginCtrl',
-                    controllerAs: 'login'    
-                }
+          parent: 'home',
+          views: {
+            'loginView@home': {
+              templateUrl: 'login/login.tpl.html',
+              controller: 'LoginCtrl',
+              controllerAs: 'login'
             }
+          }
         });
-    //   .state('login', {
-    //     url: '/login',
-    //     templateUrl: 'login/login.tpl.html',
-    //     controller: 'LoginCtrl',
-    //     controllerAs: 'login'
-    //   });
   }
 }());
